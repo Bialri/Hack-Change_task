e38bb90aef8a79992f146ed38185427df5f2c8c4
Bialri <62287690+Bialri@users.noreply.github.com>
2022-11-12 20:32:21 +0300

    added donatepage, mainpage, tarif

---
+++ vue_project/src/components/mainpage.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="heade"><svg version="1.0" class="logo" xmlns="http://www.w3.org/2000/svg"
+                          width="3295.000000pt" height="965.000000pt" viewBox="0 0 3295.000000 965.000000"
+                          preserveAspectRatio="xMidYMid meet">
+
+    <g transform="translate(0.000000,965.000000) scale(0.100000,-0.100000)"
+       fill="#FFFFFF" stroke="none">
+      <path d="M3270 9320 c-102 -13 -238 -53 -324 -96 -213 -107 -367 -304 -499
+-639 -44 -112 -1877 -5323 -1877 -5336 0 -6 229 -8 606 -7 l606 3 194 570
+c107 314 201 589 210 613 l16 42 1161 0 1162 0 194 -615 194 -615 644 0 c385
+0 643 4 643 9 0 22 -1776 5306 -1816 5406 -161 394 -367 587 -694 650 -104 19
+-319 27 -420 15z m519 -2557 c211 -665 386 -1216 389 -1225 4 -17 -39 -18
+-807 -18 -446 0 -811 3 -811 7 0 5 784 2344 815 2431 2 6 10 12 17 12 9 0 145
+-414 397 -1207z"/>
+      <path d="M7470 6250 l0 -3040 1173 0 c674 0 1239 4 1330 10 672 43 1207 230
+1686 588 147 109 404 364 504 499 350 473 519 999 558 1733 32 611 -65 1215
+-269 1677 -343 775 -1045 1321 -1927 1498 -360 72 -422 75 -1817 75 l-1238 0
+0 -3040z m2550 1776 c372 -57 654 -187 887 -409 309 -296 461 -685 488 -1252
+25 -508 -103 -978 -354 -1302 -265 -343 -644 -545 -1110 -592 -58 -6 -334 -11
+-632 -11 l-529 0 0 1788 c0 984 3 1792 8 1796 4 5 261 6 572 3 447 -4 587 -8
+670 -21z"/>
+      <path d="M29740 8470 l0 -820 -515 0 -515 0 0 -580 0 -580 515 0 514 0 4
+-1072 c3 -1045 4 -1076 25 -1183 61 -315 166 -531 339 -695 167 -160 353 -246
+648 -302 104 -20 152 -21 853 -25 l742 -5 0 610 0 610 -457 5 c-393 4 -468 7
+-528 22 -233 58 -328 182 -364 475 -7 52 -11 367 -11 822 l0 738 680 0 680 0
+0 580 0 580 -680 0 -680 0 0 820 0 820 -625 0 -625 0 0 -820z"/>
+      <path d="M15430 7750 c-457 -44 -837 -200 -1180 -488 -417 -349 -667 -802
+-762 -1382 -19 -118 -22 -173 -23 -445 0 -352 9 -441 75 -701 105 -414 293
+-739 602 -1039 184 -180 338 -289 553 -391 438 -208 1002 -260 1505 -137 526
+128 1010 488 1300 968 156 258 255 548 307 900 27 186 24 630 -6 824 -83 539
+-289 952 -652 1308 -322 315 -708 505 -1159 568 -140 19 -430 27 -560 15z
+m478 -1229 c376 -110 619 -461 664 -957 15 -163 1 -344 -37 -503 -76 -311
+-274 -566 -532 -682 -187 -85 -463 -95 -663 -24 -185 66 -362 225 -480 431
+-197 345 -212 852 -35 1221 138 289 345 464 625 529 89 21 367 12 458 -15z"/>
+      <path d="M21030 7725 c-440 -73 -838 -338 -1037 -692 l-38 -68 -3 343 -2 342
+-620 0 -620 0 0 -2220 0 -2220 620 0 619 0 4 1258 c3 1232 3 1259 24 1347 26
+115 70 226 124 318 52 88 193 231 271 275 252 143 628 135 860 -17 192 -126
+319 -341 363 -615 13 -77 15 -288 15 -1328 l0 -1238 631 0 630 0 -4 1423 c-3
+1268 -6 1435 -21 1541 -38 267 -99 475 -200 681 -239 486 -603 773 -1091 861
+-125 22 -415 27 -525 9z"/>
+      <path d="M23893 6303 c3 -1215 5 -1359 21 -1463 65 -435 185 -739 401 -1015
+82 -106 248 -263 366 -348 115 -82 358 -204 499 -251 115 -38 292 -77 440 -98
+142 -19 564 -17 711 5 332 49 605 151 850 316 439 295 693 727 784 1333 29
+194 35 444 35 1656 l0 1212 -630 0 -630 0 0 -1233 c0 -1343 1 -1324 -57 -1497
+-45 -136 -99 -226 -198 -326 -75 -77 -104 -98 -175 -132 -118 -56 -217 -77
+-370 -77 -84 0 -151 6 -205 19 -336 76 -544 357 -585 791 -6 68 -10 554 -10
+1283 l0 1172 -625 0 -626 0 4 -1347z"/>
+      <path d="M440 970 l0 -610 2945 0 2945 0 0 610 0 610 -2945 0 -2945 0 0 -610z"/>
+    </g>
+  </svg>
+    <div class="sign_in">Личный кабинет</div>
+    <div class="tarifs">Тарифы</div>
+    <div class="contacts">Контакты</div>
+  </div>
+
+  <div class="mail">
+    <h1>Напишите нам</h1>
+    <p>ADonut@thebest.ru</p>
+  </div>
+
+  <div class="socials">
+    <h1>Вступайте в группу</h1>
+    <p>@ADonut_the_best</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "mainpage"
+}
+</script>
+
+<style scoped>
+@font-face {
+  font-family: 'Styrene A Web';
+  src: local('Styrene A Web Bold'), local('Styrene-A-Web-Bold'),
+  url('../fonts/StyreneAWeb-Bold.woff2') format('woff2'),
+  url('../fonts/StyreneAWeb-Bold.woff') format('woff'),
+  url('../fonts/StyreneAWeb-Bold.ttf') format('truetype');
+  font-weight: 700;
+  font-style: normal;
+}
+*{
+  margin: 0;
+  padding: 0;
+  left: 0;
+  top: 0;
+}
+.heade{
+  position: absolute;
+  width: 100%;
+  height: 120px;
+  margin: 0;
+
+  background: #EF3124;
+}
+.logo{
+  margin-top: 10px;
+  margin-left: 10px;
+  width: 300px;
+  height: 100px;
+}
+.sign_in{
+  position: absolute;
+  width: 300px;
+  height: 50px;
+  left: 80%;
+  top: 35%;
+  font-family: "Styrene A Web";
+  font-size: 30px;
+  color: #FFFFFF;
+}
+.tarifs{
+  position: absolute;
+  width: 300px;
+  height: 50px;
+  left: 65%;
+  top: 35%;
+  font-family: "Styrene A Web";
+  font-size: 30px;
+  color: #FFFFFF;
+}
+.contacts{
+  position: absolute;
+  width: 300px;
+  height: 50px;
+  left: 50%;
+  top: 35%;
+  font-family: "Styrene A Web";
+  font-size: 30px;
+  color: #FFFFFF;
+}
+.mail{
+  position: absolute;
+  width: 600px;
+  height: 250px;
+  left: 120px;
+  top: 650px;
+
+  background: #D9D9D6;
+  border-radius: 20px;
+  text-align: center;
+}
+.mail h1{
+  margin-top: 20px;
+  font-family: "Styrene A Web";
+  font-size: 50px;
+  margin-bottom: 40px;
+}
+.mail p{
+  font-family: "Styrene A Web";
+  font-size: 30px;
+}
+
+.socials{
+  position: absolute;
+  width: 600px;
+  height: 250px;
+  left: 770px;
+  top: 650px;
+
+  background: #D9D9D6;
+  border-radius: 20px;
+  text-align: center;
+}
+.socials h1{
+  margin-top: 20px;
+  font-family: "Styrene A Web";
+  font-size: 50px;
+  margin-bottom: 40px;
+}
+.socials p{
+  font-family: "Styrene A Web";
+  font-size: 30px;
+}
+
+</style>
